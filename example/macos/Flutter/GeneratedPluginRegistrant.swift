017e43bfbd2cec8a622144527fceabca8053e4b6
Dokotela <grey.faulkenberry@mayjuun.com>
2025-02-04 13:50:04 -0500

    Not sure why swift needed updates

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_passkey
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterPasskeyMacos.register(with: registry.registrar(forPlugin: "FlutterPasskeyMacos"))
+}
